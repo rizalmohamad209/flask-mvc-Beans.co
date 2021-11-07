d6bab3eb511609866c868ba57dd054f858f52fac
rizalmohamad209 <rizalmohamad209@gmail.com>
2021-11-08 06:52:10 +0700

    change data type in konten

--- app/models/newsModel.py
+++ app/models/newsModel.py
@@ -7,7 +7,7 @@ db = SQLAlchemy(app)
 class News(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
     title = db.Column(db.String(100), nullable=False)
-    konten = db.Column(db.String(100), nullable=False)
+    konten = db.Column(db.Text, nullable=False)
     imaage = db.Column(db.String(100), nullable=False)
 
     def __init__(self, title, konten, imaage):
