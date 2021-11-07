3b9a77c00b02ac5099cea2cba30a6cbe51e9e38f
rizalmohamad209 <rizalmohamad209@gmail.com>
2021-11-08 06:59:20 +0700

    change database url

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask  # import Flask
 
 
 app = Flask(__name__,template_folder='template')
-app.config["SQLALCHEMY_DATABASE_URI"] ='postgresql://yigsixorqpqayl:cad481ec2d71a0ceb5fc6ebb8ea20c44a65c0bbe4f5dd424e89e1357099b6646@ec2-54-145-49-132.compute-1.amazonaws.com:5432/d2qa5s6jf0t5jt'
+app.config["SQLALCHEMY_DATABASE_URI"] ='postgresql://vqweqxctlhtdui:343a3b2e105a6561d318288e85a8e4b167e45569008828e0e9810f95efb82ba7@ec2-54-145-49-132.compute-1.amazonaws.com:5432/d1cug2tsr3c6v4'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 from app.controllers import *
 
